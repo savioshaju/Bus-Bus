509c82550d8147e1921d305873e66f15dad07130
Dona Mary Shaju <savioshaju>
2025-02-22 19:20:14 +0530

    Added all files

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\Savio Shaju\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Savio Shaju\\bus_bus" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\Savio Shaju\\flutter"
+  "PROJECT_DIR=D:\\Savio Shaju\\bus_bus"
+  "FLUTTER_ROOT=D:\\Savio Shaju\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Savio Shaju\\bus_bus\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Savio Shaju\\bus_bus"
+  "FLUTTER_TARGET=D:\\Savio Shaju\\bus_bus\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Savio Shaju\\bus_bus\\.dart_tool\\package_config.json"
+)
